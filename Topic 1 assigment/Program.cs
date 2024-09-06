ebb046a2fbf9ad1bc501a9aa680789c17dcb050d
North Harrington <339713067@GoTVDSB.ca>
2024-09-06 09:18:24 -0400

    Finished

--- Topic 1 assigment/Program.cs
+++ Topic 1 assigment/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Teacher!");
+Console.WriteLine("Its North");
+//Coments are usefull because you can send a message or explane what the line of code does
+//Im the goat at this activity, These are my initials?
+Console.WriteLine(" N       N      H       H"); 
+Console.WriteLine(" N N     N      H       H"); 
+Console.WriteLine(" N   N   N      H       H"); 
+Console.WriteLine(" N     N N      H H H H H" ); 
+Console.WriteLine(" N       N      H       H"); 
+Console.WriteLine(" N       N      H       H"); 
+Console.WriteLine(" N       N      H       H");
+Console.ReadLine();
